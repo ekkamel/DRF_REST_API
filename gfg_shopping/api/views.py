998705cb9c291c7cd4471737a857972a869a4a4c
Emad Kamel <ekkamel@gmail.com>
2023-09-07 23:55:19 +0300

    added post

--- gfg_shopping/api/views.py
+++ gfg_shopping/api/views.py
@@ -3,11 +3,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Item 
 from .serializers import ItemSerializer
+from rest_framework import serializers
+from rest_framework import status 
 
 
 # Create your views here.
 @api_view(['GET'])
 def ApiOverview(request): 
+        
     api_urls = {
         'all_items': '/', 
         'Search by Category': '/?category=category_name', 
@@ -18,3 +21,17 @@ def ApiOverview(request):
     }
     
     return Response(api_urls)
+
+@api_view(['POST'])
+def add_items(request): 
+    item = ItemSerializer(data=request.data)
+    
+    # validating for already existing data 
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data alreay exists')
+    
+    if item.is_valid(): 
+        item.save()
+        return Response(item.data)
+    else: 
+        return Response(status=status.HTTP_404_NOT_FOUND)
